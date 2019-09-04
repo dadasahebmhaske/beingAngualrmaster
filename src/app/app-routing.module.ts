b8ccad0991cada9c754cec7b95a73f935577bf38
dmask <dadasaheb41@gmail.com>
2019-09-04 17:12:32 +0530

    add dashboard user and departmnet component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DepartmentComponent } from './department/department.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserlistComponent } from './userlist/userlist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  // {
+  //   path: 'recipes',
+  //   component: RecipesComponent,
+  //   children: [
+  //     { path: '', component: RecipeStartComponent },
+  //     { path: 'new', component: RecipeEditComponent },
+ 
+  
+  //   ]
+  // },
+  { path: 'department', component: DepartmentComponent },
+  { path: 'userlist', component: UserlistComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
